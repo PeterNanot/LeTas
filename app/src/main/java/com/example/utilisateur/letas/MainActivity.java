9a32c1fd46212e41efc87c524084852c76bab38a
Peter Nanot <Peter Nanot>
2017-01-19 21:27:52 +0100

    Mise en place

--- app/src/main/java/com/example/utilisateur/letas/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/letas/MainActivity.java
@@ -3,21 +3,41 @@ package com.example.utilisateur.letas;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
+    int i = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView carte = (ImageView) findViewById(R.id.imageView);
-        Drawable drawable = findViewById(R.drawable.carte);
-        carte.setImageDrawable();
+        final ImageView imageContainer = (ImageView) findViewById(R.id.imageView);
+        final ArrayList<Integer> list = new ArrayList<>();
+        list.add(R.drawable.carte);
+        list.add(R.drawable.human_reference);
+        Collections.shuffle(list);
+        imageContainer.setImageResource(list.get(0));
+
+        imageContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i == list.size() + 1) {
+                    i = 0;
+                }
+                imageContainer.setImageResource(list.get(i));
+            }
+        });
+
+
     }
 
 
